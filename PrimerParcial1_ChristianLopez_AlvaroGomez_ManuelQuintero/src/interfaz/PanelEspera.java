ad37f2eac752c386f0bf1023f83477488b54197c
manuelq12 <36927617+manuelq12@users.noreply.github.com>
2019-03-22 19:00:15 -0500

    PanelEspera
    Creación del Panel donde se espera a los jugadores ingresar

--- PrimerParcial1_ChristianLopez_AlvaroGomez_ManuelQuintero/src/interfaz/PanelEspera.java
+++ PrimerParcial1_ChristianLopez_AlvaroGomez_ManuelQuintero/src/interfaz/PanelEspera.java
@@ -1,5 +1,29 @@
 package interfaz;
 
-public class PanelEspera {
+import java.awt.*;
 
+import javax.swing.*;
+
+public class PanelEspera extends JPanel{
+
+	private JLabel banner;
+	
+	
+	public static void main(String[] args) {
+		JFrame x = new JFrame();
+		PanelEspera n = new PanelEspera();
+		x.add(n);
+		x.setVisible(true);
+		n.setVisible(true);
+	    x.pack();
+	}
+	public PanelEspera() {
+		setLayout(new BorderLayout());
+		setBackground(Color.BLACK);
+		banner = new JLabel();
+		ImageIcon bn = new ImageIcon("../docs/imgs/bannerEspera.jpg");
+		banner.setIcon(bn);
+		this.add(banner, BorderLayout.NORTH);
+		
+	}
 }
